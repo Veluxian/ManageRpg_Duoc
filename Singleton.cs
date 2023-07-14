98a45a059e9da7ea1a9996bc2e67c6a17d08752f
Matteo D. Med <108637898+MatteoArMed@users.noreply.github.com>
2023-07-13 21:34:31 -0400

    Add Singleton

---
+++ Singleton.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ManageRPG.Modelo
+{
+public class DatabaseConnection
+    {
+        private static DatabaseConnection _instance;
+        private SqlConnection _connection;
+
+        private DatabaseConnection()
+        {
+            string connectionString = "Server=SAGITTARIUS\\SQLEXPRESS;Database=ManageRPG;Integrated Security=true";
+            _connection = new SqlConnection(connectionString);
+        }
+
+        public static DatabaseConnection Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new DatabaseConnection();
+                }
+                return _instance;
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return _connection;
+        }
+
+        public void OpenConnection()
+        {
+            if (_connection.State != System.Data.ConnectionState.Open)
+            {
+                _connection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (_connection.State != System.Data.ConnectionState.Closed)
+            {
+                _connection.Close();
+            }
+        }
+    }
+}
